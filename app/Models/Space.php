51657348cbce04f1ae8cc46c57581012ff9b7636
EC2 Default User <ec2-user@ip-172-31-43-167.ap-northeast-1.compute.internal>
2023-10-21 06:05:07 +0000

    model method

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -39,8 +39,19 @@ class Space extends Model
         $kafka_damage = $kafka->value('damage_parsent') / 100;
         $kafka_crit = $kafka->value('crit_damage') / 100 + 1;
         
+        $nomalhit = $kafka_attack *$kafka_damage;
+        $crithit = $kafka_attack * $kafka_damage * $kafka_crit;
+        
         $kafka_dot = $kafka_attack * 2.9;
-        return $kafka_dot;
+        $skill_nomal = $nomalhit * 1.6;
+        $skill_crit = $crithit * 1.6;
+        $skill_dot = $kafka_dot * 0.75;
+        $ult_nomal = $nomalhit * 0.8;
+        $ult_crit = $crithit * 0.8;
+        $passive_nomal = $nomalhit * 1.4;
+        $passive_crit = $crithit * 1.4;
+        
+        return [$nomalhit, $crithit, $skill_nomal, $skill_crit, $ult_nomal, $ult_crit, $kafka_dot, $skill_dot, $passive_nomal, $passive_crit];
     }
     
     public function Seele() {
@@ -49,9 +60,15 @@ class Space extends Model
         $seele_damage = $seele->value('damage_parsent') / 100;
         $seele_crit = $seele->value('crit_damage') / 100 + 1;
         
-        $seele_nomalhit = $seele_attack * $seele_damage;
-        $seele_crithit = $seele_nomalhit * $seele_crit;
-        return [$seele_nomalhit, $seele_crithit];
+        $nomalhit = $seele_attack * $seele_damage;
+        $crithit = $seele_nomalhit * $seele_crit;
+        $passive = 1.8;
+        
+        $skill_nomal = $nomalhit * 2.2;
+        $skil_crit = $crithit * 2.2;
+        $ult_nomal = ($nomalhit + $passive) * 4.25;
+        $ult_crit = ($crithit + $passive) * 4.25;
+        return [$nomalhit, $crithit, $skill_nomal, $skil_crit, $ult_nomal, $ult_crit];
     }
     
     public function ImbibitorLunae() {
@@ -60,8 +77,8 @@ class Space extends Model
         $lunae_damage = $lunae->value('damage_parsent') / 100;
         $lunae_crit = $lunae->value('crit_damage') / 100 + 1;
         
-        $lunae_nomalhit = $lunae_attack * $lunae_damage;
-        $lunae_crithit = $lunae_nomalhit * $lunae_crit;
+        $nomalhit = $lunae_attack * $lunae_damage;
+        $crithit = $lunae_nomalhit * $lunae_crit;
     }
     
     public function Jingliu() {
